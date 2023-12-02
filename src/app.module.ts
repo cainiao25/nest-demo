e1960b8764296275102784aa862a76aae1bd6836
“liliangcan” <2633057734@qq.com>
2023-12-02 14:36:58 +0800

    feat: 关于env的暂提

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,50 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+import { ConfigEnum } from './enum/config.enum';
+import * as dotenv from 'dotenv'
+
+// const envFilePath = `.env.${process.env.NODE_ENV || 'development'}`
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      // envFilePath,
+      // load: [() => dotenv.config({ path: '.env'})]
+    }), //.forRoot()是读取根目录的.env文件  isGlobal: true 开启为true就可以全局使用， 可以在private上引用。
+    // TypeOrmModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   inject: [ConfigModule],
+    //   useFactory: (configService: ConfigService) => ({
+    //     type: 'mysql',
+    //     host: configService.get(ConfigEnum.DB_HOST),
+    //     port: configService.get(ConfigEnum.DB_PORT),
+    //     username: configService.get(ConfigEnum.DB_USERNAME),
+    //     password: configService.get(ConfigEnum.DB_PASSWORD),
+    //     synchronize: configService.get(ConfigEnum.DB_SYNC),
+    //     database: configService.get(ConfigEnum.DB_DATABASE),
+    //     entities: [],
+    //     //同步本地的schema与数据库 -》 初始化的时候去使用
+    //     logging: ['error']
+    //   } as TypeOrmModuleAsyncOptions)
+    // })
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123456',
+      synchronize: true,
+      database: 'mysql',
+      entities: [],
+      //同步本地的schema与数据库 -》 初始化的时候去使用
+      logging: ['error']
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+//nest 就会从 AppModule 开始解析 class 上通过装饰器声明的依赖信息，自动创建和组装对象。
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 给请求地址添加前缀
+  app.setGlobalPrefix('api/v1');
   await app.listen(3000);
 }
 bootstrap();
