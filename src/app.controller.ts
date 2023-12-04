e964bf45ca357c962dff20e87d2cc5ebfe98d02c
“liliangcan” <2633057734@qq.com>
2023-12-04 11:01:20 +0800

    feat: IoC机制

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// Controller 是单独的装饰器
 @Controller()
 export class AppController {
+  // AppController 的构造器参数依赖了 AppService。
   constructor(private readonly appService: AppService) {}
+  // 所以 AppController 只是声明了对 AppService 的依赖，就可以调用它的方法了
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHello(); // 调用AppService的方法
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+//那么 nest 就会从 AppModule
+// 开始解析 class 上通过装饰器声明的依赖信息，自动创建和组装对象。
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController], // controllers 是控制器，只能被注入。
+  providers: [AppService], //providers 里可以被注入，也可以注入别的对象，比如这里的 AppService。
 })
 export class AppModule {}
