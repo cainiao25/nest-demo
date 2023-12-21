8819cab0f7f8b8ca3527fac2e55e7b70650064cd
“liliangcan” <2633057734@qq.com>
2023-12-21 16:49:33 +0800

    feat: 自定义装饰器暂提

---
+++ src/aaa.guard.ts
@@ -0,0 +1,17 @@
+import { CanActivate, ExecutionContext, Injectable, Inject } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AaaGuard implements CanActivate {
+  @Inject(Reflector)
+  private reflector: Reflector;
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+
+    console.log(this.reflector.get('aaa', context.getHandler()));
+
+    return true;
+  }
+}
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  SetMetadata,
+  UseGuards
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { Aaa } from "./aaa.decorator";
+import { AaaGuard } from './aaa.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  // @SetMetadata('aaa', 'admin')
+  @Aaa('admin')
+  @UseGuards(AaaGuard)
   getHello(): string {
     return this.appService.getHello();
   }
