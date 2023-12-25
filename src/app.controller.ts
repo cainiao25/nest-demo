3ab90b27cd4509dc44a0882d9a29ce44fb61f2f2
“liliangcan” <2633057734@qq.com>
2023-12-25 17:26:46 +0800

    feat: 自定义Exception Filter + 自定义 Exception

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Post, Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import {AaaDto } from './aaa.dto'
+import { UnLoginException } from './unlogin.filter';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
+    // throw new HttpException('xxxx', HttpStatus.BAD_REQUEST)
+    throw new UnLoginException()
     return this.appService.getHello();
+  }
+
+  @Post('aaa')
+  aaa(@Body () aaaDto: AaaDto){
+    console.log(aaaDto,'aaaDto');
+    return 'success'
   }
 }
